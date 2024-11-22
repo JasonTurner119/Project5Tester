37eeb55a1ee98d68980150d94fee89b4f89ef955
Jason Turner <auroral.tellers_00@icloud.com>
2024-11-22 10:52:12 -0800

    Add logging to servlet.

--- src/TomcatPoolingServlet.java
+++ src/TomcatPoolingServlet.java
@@ -11,6 +11,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.logging.Logger;
 
 @WebServlet(name = "TomcatPoolingServlet", urlPatterns = "/")
 public class TomcatPoolingServlet extends HttpServlet {
